8aceb40ce76e08e4c428436488e2f17e2a4ac4ca
selim121 <selimkhan.bdkhan@gmail.com>
2023-06-14 16:00:46 +0600

    task spark repo created with navbar

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Pages/Home/Navbar/Navbar";
+
+const Main = () => {
+    return (
+        <>
+            <div >
+                <Navbar />
+            </div>
+        </>
+    );
+};
+
+export default Main;
